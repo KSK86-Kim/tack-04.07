e98e855337eda470fbcd12925d202ac3204671da
Oleksandr Polishchuk <70407461+KSK86-Kim@users.noreply.github.com>
2021-07-06 23:10:51 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './font/Circe/stylesheet.css';
+import './scss/main.scss';
+import refs from './js/refs';
---
+++ src/js/refs.js
@@ -0,0 +1,20 @@
+const refs = {
+  searchInput: document.querySelector('.search-input'),
+  searchButton: document.querySelector('.search-button'),
+  moviesCardsGallery: document.querySelector('.movie__list'),
+  movieInfoModal: document.querySelector('.modal-movie'),
+  openModalTeamBtn: document.querySelector('.js-openModalTeam'),
+  modalOverlay: document.querySelector('.js-modalOverlay'),
+  closeModalTeamBtn: document.querySelector('.js-closeModalTeam'),
+  loadQueueBtn: document.querySelector('[data-load-queue]'),
+  loadWatchedBtn: document.querySelector('[data-load-watched]'),
+  btnNextPagination: document.querySelector('[data-next-pagination]'),
+  btnPrevPagination: document.querySelector('[data-prev-pagination]'),
+  paginationControls: document.querySelector('[data-pagination-controls-list]'),
+  modalTeam: document.querySelector('.modalTeam'),
+  modalTeamList: document.querySelector('.modalTeam__list'),
+  warningNotificationRef: document.querySelector('.warning-notification'),
+  modalFooter: document.querySelector('.modalTeam__footer'),
+  modalTitles: document.querySelector('.star-wars-wrapper'),
+};
+export default refs;
